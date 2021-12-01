82d27e242b7428147415179907a71ac23a128aef
Gabrielle Santos <gabiecarvalho98@gmail.com>
2021-12-01 18:26:56 -0300

    pag cadastro usuario

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,12 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { CadastroUsuarioComponent } from './cadastro-usuario/cadastro-usuario.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    CadastroUsuarioComponent
   ],
   imports: [
     BrowserModule,
