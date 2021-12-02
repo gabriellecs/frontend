b6ae9ad8a3c68544e846460b35a5e73b30e359f2
Gabrielle Santos <gabiecarvalho98@gmail.com>
2021-12-02 18:23:06 -0300

    ajuste  final

--- src/app/cadastro-categoria/cadastro-categoria.component.ts
+++ src/app/cadastro-categoria/cadastro-categoria.component.ts
@@ -1,4 +1,7 @@
 import { Component, OnInit } from '@angular/core';
+import { FormBuilder, FormGroup, Validators } from '@angular/forms';
+import { ActivatedRoute, Router } from '@angular/router';
+import { Cliente } from '../models/cliente.model';
 
 @Component({
   selector: 'app-cadastro-categoria',
@@ -6,10 +9,41 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./cadastro-categoria.component.css']
 })
 export class CadastroCategoriaComponent implements OnInit {
+  id: number;
+  cliente: Cliente;
+  form: FormGroup;
+  nomeFormControl = this.fb.control('', { validators: [Validators.required], updateOn: 'blur' });
+  descricaoFormControl = this.fb.control('', { validators: [Validators.required], updateOn: 'blur' });
+  emailFormControl: any;
+  categoriaService: any;
+ 
+  
+  
+ 
 
-  constructor() { }
-
-  ngOnInit(): void {
+  
+  constructor(private router: Router,
+    private fb: FormBuilder,
+    private route: ActivatedRoute,) {
+    this.form = this.fb.group({
+      nomeFormControl: this.nomeFormControl,
+      cpfFormControl: this.descricaoFormControl,
+    });
   }
 
+
+  ngOnInit(): void {
+    let id: string;
+    id = this.route.snapshot.paramMap.get("id");
+    if (id != undefined || id != null) {
+      this.id = Number.parseInt(id);
+      this.categoriaService.getPeloId(this.id).subscribe((cadastro: Cliente) => {
+        this.nomeFormControl.setValue(cadastro.nome);
+        this.descricaoFormControl.setValue(cadastro.nome);
+
+
+      })
+    }
+  }
+  
 }
